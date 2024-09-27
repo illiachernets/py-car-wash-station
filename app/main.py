d43d10e50d9ab8ba5b156a414d5acfa61303b544
Illia <ilyachernets@gmail.com>
2024-09-27 16:30:07 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 class Car:
-    def __init__(
-                self,
-                comfort_class: int,
-                clean_mark: int,
-                brand: str
-                ) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
 
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -12,13 +10,11 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(
-                self,
-                distance_from_city_center: float,
-                clean_power: int,
-                average_rating: float,
-                count_of_ratings: int
-                ) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -43,7 +39,7 @@ class CarWashStation:
 
         self.count_of_ratings += 1
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             if self.clean_power > car.clean_mark:
